f90edfdacf8723d37c40aafbb4e162f81d6ebe17
esphoenixc <bbcd6222@gmail.com>
2024-03-19 19:21:22 +0900

    add: sign in feature 기능 작업을 위한 임시 UI 작업

--- Syrup/Views/SignInViewController.swift
+++ Syrup/Views/SignInViewController.swift
@@ -2,10 +2,65 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    private let googleSignInButton = {
+        let button = UIButton()
+        button.setTitle("Google Login", for: .normal)
+        button.layer.cornerRadius = 20
+        button.layer.borderWidth = 1
+        return button
+    }()
+    
+    private let appleSignInButton = {
+        let button = UIButton()
+        button.setTitle("Apple Login", for: .normal)
+        button.layer.cornerRadius = 20
+        button.layer.borderWidth = 1
+        
+        return button
+    }()
+    
+    //TODO: View
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemCyan
+        configureUI()
+        configureUIApple()
+    }
+    
+
+    //TODO: View
+    private func configureUI() {
+        view.addSubview(googleSignInButton)
+        googleSignInButton.translatesAutoresizingMaskIntoConstraints = false
+        googleSignInButton.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        googleSignInButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        googleSignInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        googleSignInButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        
+        googleSignInButton.addTarget(self, action: #selector(onGoogleSignInTapped), for: .touchUpInside)
+    }
+    
+    //TODO: View
+    private func configureUIApple() {
+        view.addSubview(appleSignInButton)
+        appleSignInButton.translatesAutoresizingMaskIntoConstraints = false
+        appleSignInButton.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        appleSignInButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        appleSignInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        appleSignInButton.topAnchor.constraint(equalTo: googleSignInButton.bottomAnchor, constant: 20).isActive = true
+        
+        appleSignInButton.addTarget(self, action: #selector(onAppleSignInTapped), for: .touchUpInside)
+    }
+    
+    //TODO: ViewModel
+    @objc private func onAppleSignInTapped() {
+        print("Apple")
+    }
+    
+    //TODO: ViewModel
+    @objc private func onGoogleSignInTapped() {
+        print("Google")
     }
 }
 
